2fd1dd81757676dbbd6fcb77d90cef4ee24a7efb
RubenLeszczynski <ruben.leszczynski@stu.ocsb.ca>
2022-09-21 14:36:45 -0400

    Update Chickens01.java

--- src/Chickens01.java
+++ src/Chickens01.java
@@ -2,6 +2,25 @@ public class Chickens01 {
     public static void main(String[] args) {
         //Put your code here
         
+    	
+    	int totalEggs = 0;
+        int eggsPerChicken = 3;
+        int chickenCount = 5;
+        //Monday
+        totalEggs = eggsPerChicken * chickenCount;
+        //Tuesday
+        chickenCount++;
+        totalEggs += eggsPerChicken *chickenCount;
+        //Wednesday
+        chickenCount /= 2;
+        totalEggs += eggsPerChicken * chickenCount;
         System.out.println(totalEggs);
+      
+
+        
+        
+        
+        
+        
     }   
 }
