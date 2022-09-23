eead43307d3f721818637152b27dad9b9f4e0db4
RubenLeszczynski <ruben.leszczynski@stu.ocsb.ca>
2022-09-23 14:35:58 -0400

    Update Chickens02.java

--- src/Chickens02.java
+++ src/Chickens02.java
@@ -1,10 +1,36 @@
 public class Chickens02 {
     public static void main(String[] args) {
         //Put your code here
-
-        System.out.println("Daily Average:   " +dailyAverage);
-        System.out.println("Monthly Average: " +monthlyAverage);
-        System.out.println("Monthly Profit:  $" +monthlyProfit);
+    	int dailyAverage = 0;
+    	int monthlyAverage = 0;
+    	double monthlyProfit = 0;
+    	int eggCount = 0;
+    	//Monday
+    	eggCount+=100; //Collect 100 eggs
+    	//Tuesday
+    	eggCount+=121; //Collect 121 eggs
+    	//Wednesday
+    	eggCount+=117; //Collect 117 eggs
+    	
+    	//calculate Daily Average
+    	dailyAverage = eggCount/3;
+    	System.out.println("Daily Average:   " +dailyAverage);
+    	
+    	//calculate Monthly Average
+    	monthlyAverage = dailyAverage*30;
+    	System.out.println("Monthly Average: " +monthlyAverage);
+    
+    	//calculate Monthly Profit
+    	monthlyProfit = monthlyAverage*0.18;
+    	System.out.println("Monthly Profit:  $" +monthlyProfit);
+    	
+    	
+    	
+    	
+        
+        
+        
+        
     }
     
 }
